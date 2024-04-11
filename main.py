b48b9a67fde9e481bda49fa2960d3b0fda128cf3
Alexander Heard <heardalhe@gmail.com>
2024-04-11 13:47:05 -0400

    hi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#hi
+#Shane Wooden
+
 def decode(encoded_password):
     decoded_password=""
     for character in encoded_password:
@@ -11,3 +12,30 @@ def decode(encoded_password):
             value_to_add=changed_value-3
             decoded_password+=str(value_to_add)
     return decoded_password
+
+def encode(password):
+    new_password = ''
+    for number in password:
+        if number == '9':
+            new_password += '2'
+        if number == '8':
+            new_password += '1'
+        if number == '7':
+            new_password += '0'
+        else:
+            new_password += str(int(number)+3)
+    return new_password
+
+if __name__ == '__main__':
+    while True:
+        print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
+        option = int(input('Please enter an option: '))
+        if option == 3:
+            break
+        if option == 1:
+            password = str(input('Please enter your password to encode: '))
+            encoded_password = encode(password)
+            print('Your password has been encoded and stored!')
+        if option == 2:
+            decoded_password = decode(encoded_password)
+            print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.')
