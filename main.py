9c8806d6a0263c0f51ff6e7a67b989bb7099911b
Shane Wooden <shanewooden77@gmail.com>
2024-04-11 13:20:24 -0400

    first

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #hi
+def decode(encoded_password):
+    decoded_password=""
+    for character in encoded_password:
+        if int(character)>=3:
+            value_to_add=int(character)-3
+            value_to_add=str(value_to_add)
+            decoded_password+=value_to_add
+        elif int(character)<3:
+            changed_value=int(character)+10
+            value_to_add=changed_value-3
+            decoded_password+=str(value_to_add)
+    return decoded_password
